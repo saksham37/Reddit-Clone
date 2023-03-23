734398e6837b4e3cccb2638f0396fb40e253f4b6
Saksham Sharma <saksham.cse23@gmail.com>
2023-03-24 02:33:16 +0530

    add comments

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './Navbar/Navbar';
 
-const Layout:React.FC = ({children}) => {
-    return (
-        <>
-        {/* {Navbar} */}
-        <main>{children} </main>
-        </>
-    )
+interface LayoutProps {
+  children: React.ReactNode;
 }
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+
 export default Layout;
